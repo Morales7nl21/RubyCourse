6adb8bd02107b0de38569abecab35a27177453db
Morales7nl21 <thedjdave98@gmail.com>
2021-07-03 17:26:37 -0500

    Modificando primer programa

--- L1/iniciandoRuby.rb
+++ L1/iniciandoRuby.rb
@@ -1,7 +1,6 @@
-3.times { puts "Hello World" }
+2.times { puts "Hello World" }
+# comentario
+puts 5 
+3 
 
-# this is a comment 
-puts 5 # so is this
-3 # and this
-
-p "Got it" # => Got it
+p "Got it" # imprime el texto 
